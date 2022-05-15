c0d78367f5227133997015c9b7db4124044ac84b
strlla <estrellaportocarrero@gmail.com>
2022-05-15 15:25:20 -0300

    Calculate lucky number

---
+++ src/components/LuckyNumber/index.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import "./styles.scss";
+
+const LuckyNumber = ({ name }) => {
+  const [luckyNumber, setLuckyNumber] = useState(0);
+
+  const alpha = Array.from(Array(27)).map((e, i) => i + 64);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  const calculateLuckyNumber = () => {
+    const letters = name.split("");
+    return letters.reduce(
+      (acc, letter) => acc + alphabet.indexOf(letter.toUpperCase()),
+      0
+    );
+  };
+  
+  useEffect(() => {
+    if (name) {
+      setLuckyNumber(calculateLuckyNumber());
+    }
+  }, [name]);
+
+  return (
+    <div className="luckyNumber">
+      <p>Lucky Number: {luckyNumber}</p>
+    </div>
+  );
+};
+
+export default LuckyNumber;
